f15b823c67bcc2798877e0858f93428f4c03025e
saresend <saresend@usc.edu>
2020-04-25 16:50:45 -0700

    defining SQL types

---
+++ server/migrations/2020-04-25-232116_create_tables/down.sql
@@ -0,0 +1,4 @@
+-- This file should undo anything in `up.sql`
+DROP TABLE Appointments;
+DROP TABLE Users;
+DROP TABLE Locations;
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,71 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate diesel;
+
+mod schema;
+use crate::schema::*;
+use diesel::prelude::*;
+use diesel::sqlite::SqliteConnection;
+use dotenv::dotenv;
+use lazy_static::lazy_static;
+use std::env;
+use std::sync::Mutex;
+
+lazy_static! {
+    static ref CONN: Mutex<SqliteConnection> = Mutex::new(get_connection());
 }
+
+#[derive(Queryable)]
+pub struct Location {
+    pub id: i32,
+    pub name: String,
+    pub lat: f32,
+    pub lng: f32,
+}
+
+#[derive(Insertable)]
+#[table_name = "Locations"]
+pub struct NewLocation {
+    pub name: String,
+    pub lat: f32,
+    pub lng: f32,
+}
+
+#[derive(Queryable)]
+pub struct User {
+    pub id: i32,
+    pub email: String,
+    pub token: String,
+}
+
+#[derive(Insertable)]
+#[table_name = "Users"]
+pub struct NewUser {
+    pub email: String,
+    pub token: String,
+}
+
+#[derive(Queryable)]
+pub struct Appointment {
+    pub id: i32,
+    pub location_id: i32,
+    pub user_id: i32,
+    pub len: i32,
+    pub timestamp: String,
+}
+
+#[derive(Insertable)]
+#[table_name = "Appointments"]
+pub struct NewAppointment {
+    pub location_id: i32,
+    pub user_id: i32,
+    pub len: i32,
+    pub timestamp: String,
+}
+
+fn get_connection() -> SqliteConnection {
+    dotenv().ok();
+    let db_url = env::var("DATABASE_URL").unwrap();
+    SqliteConnection::establish(&db_url).expect("Error connecting")
+}
+
+fn main() {}
