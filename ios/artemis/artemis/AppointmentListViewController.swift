b214fe82e7e8a8a2da2f075f337516e712b8d6c3
saresend <saresend@usc.edu>
2020-04-25 15:06:29 -0700

    now supports a list of scheduled appointments

---
+++ ios/artemis/artemis/AppointmentListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  AppointmentListViewController.swift
+//  artemis
+//
+//  Created by Samuel Resendez on 4/25/20.
+//  Copyright © 2020 Samuel Resendez. All rights reserved.
+//
+
+import UIKit
+
+class AppointmentListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "appointCell") as! AppointmentTableViewCell
+        cell.titleLabel.text = "Trader Joes"
+        cell.dateLabel.text = "May 5th, 2020"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    @IBOutlet var appointmentTableView: UITableView!
+    
+    override func viewDidLoad() {
+        appointmentTableView.delegate = self
+        appointmentTableView.dataSource = self
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ios/artemis/artemis/AppointmentTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  AppointmentTableViewCell.swift
+//  artemis
+//
+//  Created by Samuel Resendez on 4/25/20.
+//  Copyright © 2020 Samuel Resendez. All rights reserved.
+//
+
+import UIKit
+
+class AppointmentTableViewCell: UITableViewCell {
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
