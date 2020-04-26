85b8c87095cc0f83521715420eda00135945853a
Sophia Zheng <sophiazheng5@gmail.com>
2020-04-25 21:44:49 -0400

    Adding time slots to time collection view

---
+++ ios/artemis/artemis/TimeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TimeCollectionViewCell.swift
+//  artemis
+//
+//  Created by Sophia Zheng on 4/25/20.
+//  Copyright © 2020 Samuel Resendez. All rights reserved.
+//
+
+import UIKit
+
+class TimeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet weak var selectedLabel: UITextField!
+}
--- ios/artemis/artemis/TimeViewController.swift
+++ ios/artemis/artemis/TimeViewController.swift
@@ -8,9 +8,44 @@
 
 import UIKit
 
-class TimeViewController: UIViewController {
+class TimeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "timeCell", for: indexPath) as! TimeCollectionViewCell
+        
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute]
+        formatter.unitsStyle = .positional
+        
+        let interval = (indexPath.row + 8) * 3600
+        cell.timeLabel.text = formatter.string(from: TimeInterval(interval))!
+
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as! TimeCollectionViewCell
+        cell.selectedLabel.textColor = UIColor.lightGray
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as! TimeCollectionViewCell
+        cell.selectedLabel.textColor = UIColor.clear
+    }
+    
+    @IBOutlet weak var timeCollectionView: UICollectionView!
     override func viewDidLoad() {
+        timeCollectionView.delegate = self
+        timeCollectionView.dataSource = self
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
