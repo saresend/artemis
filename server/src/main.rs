658357ec17df29288285ba391a31400759bc671c
saresend <saresend@usc.edu>
2020-04-25 17:18:54 -0700

    adding warp functions

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,11 @@
 #[macro_use]
 extern crate diesel;
 
+#[macro_use]
+extern crate serde_derive;
+
+use warp::Filter;
+
 mod schema;
 use crate::schema::*;
 use diesel::prelude::*;
@@ -14,7 +19,7 @@ lazy_static! {
     static ref CONN: Mutex<SqliteConnection> = Mutex::new(get_connection());
 }
 
-#[derive(Queryable)]
+#[derive(Queryable, Serialize)]
 pub struct Location {
     pub id: i32,
     pub name: String,
@@ -22,7 +27,7 @@ pub struct Location {
     pub lng: f32,
 }
 
-#[derive(Insertable)]
+#[derive(Insertable, Deserialize)]
 #[table_name = "Locations"]
 pub struct NewLocation {
     pub name: String,
@@ -30,21 +35,21 @@ pub struct NewLocation {
     pub lng: f32,
 }
 
-#[derive(Queryable)]
+#[derive(Queryable, Serialize)]
 pub struct User {
     pub id: i32,
     pub email: String,
     pub token: String,
 }
 
-#[derive(Insertable)]
+#[derive(Insertable, Deserialize)]
 #[table_name = "Users"]
 pub struct NewUser {
     pub email: String,
     pub token: String,
 }
 
-#[derive(Queryable)]
+#[derive(Queryable, Serialize)]
 pub struct Appointment {
     pub id: i32,
     pub location_id: i32,
@@ -53,7 +58,7 @@ pub struct Appointment {
     pub timestamp: String,
 }
 
-#[derive(Insertable)]
+#[derive(Insertable, Deserialize)]
 #[table_name = "Appointments"]
 pub struct NewAppointment {
     pub location_id: i32,
@@ -62,10 +67,27 @@ pub struct NewAppointment {
     pub timestamp: String,
 }
 
+fn get_all_locations() -> Vec<Location> {
+    todo!()
+}
+
+fn get_appointments_for_token(token: String) -> Vec<Appointment> {
+    todo!()
+}
+
+fn insert_new_appointment(appt: &NewAppointment) {
+    todo!()
+}
+
 fn get_connection() -> SqliteConnection {
     dotenv().ok();
     let db_url = env::var("DATABASE_URL").unwrap();
     SqliteConnection::establish(&db_url).expect("Error connecting")
 }
 
-fn main() {}
+fn main() {
+    let location_path = warp::path("location/").map(|| warp::reply::json(&get_all_locations()));
+
+    let routes = warp::get().and(location_path);
+    warp::serve(routes).run(([0, 0, 0, 0], 8000));
+}
